261990016976cf9b5820e62c4ce79b7efebfff31
Ratchatrin sisan <148691205+Ratchatrin@users.noreply.github.com>
2024-06-22 16:35:50 +0700

    change  src img

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -17,8 +17,8 @@ function Home() {
           <div className="anime font-home w-full h-full flex flex-col gap-10 justify-start items-center">
             <div className="flex flex-col gap-3 justify-center items-center font-bold text-3xl text-center">
               <img
-                src="https://scontent.fcnx4-1.fna.fbcdn.net/v/t39.30808-6/448617868_7490656041060510_7200582433962848854_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=oAfENk-jmtQQ7kNvgFDcJIK&_nc_ht=scontent.fcnx4-1.fna&oh=00_AYCVLK0-RetE9C5e1JX_Pax04BShhmaZIZE2rLqCYg__EQ&oe=6675BAFE"
-                alt=""
+                src="https://i.ibb.co/ydbqNZx/306749716-746133143151985-792680549932249393-n.jpg"
+                alt="profile"
                 className="w-56 mb-5 rounded-lg"
               />
               <p>Full-Stack Developer </p>
@@ -43,8 +43,8 @@ function Home() {
           <div className="anime font-home w-full h-3/5 flex flex-col gap-10 justify-center items-center">
             <div className="flex flex-col gap-3 justify-center items-center font-bold text-4xl">
               <img
-                src="https://scontent.fcnx4-1.fna.fbcdn.net/v/t39.30808-6/448617868_7490656041060510_7200582433962848854_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=oAfENk-jmtQQ7kNvgFDcJIK&_nc_ht=scontent.fcnx4-1.fna&oh=00_AYCVLK0-RetE9C5e1JX_Pax04BShhmaZIZE2rLqCYg__EQ&oe=6675BAFE"
-                alt=""
+                src="https://i.ibb.co/ydbqNZx/306749716-746133143151985-792680549932249393-n.jpg"
+                alt="profile"
                 className="w-56 mb-5 rounded-lg"
               />
               <p>Full-Stack Developer </p>
